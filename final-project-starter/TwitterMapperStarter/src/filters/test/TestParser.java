65dee4647fd054e1d85434f0a24b201ed8343650
Christian Páez <green_pok@hotmail.com>
2020-01-19 13:17:22 -0500

    Task 3: Connect your new classes to the filter parser

--- final-project-starter/TwitterMapperStarter/src/filters/test/TestParser.java
+++ final-project-starter/TwitterMapperStarter/src/filters/test/TestParser.java
@@ -17,6 +17,18 @@ public class TestParser {
     }
 
     @Test
+    public void testOrSimple() throws SyntaxError {
+        Filter f = new Parser("trump or clinton").parse();
+        assertTrue(f.toString().equals("(trump or clinton)"));
+    }
+
+    @Test
+    public void testAndSimple() throws SyntaxError {
+        Filter f = new Parser("obama and trump").parse();
+        assertTrue(f.toString().equals("(obama and trump)"));
+    }
+
+    @Test
     public void testHairy() throws SyntaxError {
         Filter x = new Parser("trump and (evil or blue) and red or green and not not purple").parse();
         assertTrue(x.toString().equals("(((trump and (evil or blue)) and red) or (green and not not purple))"));
